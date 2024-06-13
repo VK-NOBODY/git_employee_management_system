d2b249abdad68776b1d79fb7d078c6ca8e324497
VK-NOBODY <171213515+VK-NOBODY@users.noreply.github.com>
2024-06-13 23:17:22 +0800

    commit

---
+++ cmake-build-release/CMakeFiles/git_employee_management_system.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_employee_management_system.dir/main.cpp.obj"
+  "CMakeFiles/git_employee_management_system.dir/main.cpp.obj.d"
+  "CMakeFiles/git_employee_management_system.dir/worker_manager.cpp.obj"
+  "CMakeFiles/git_employee_management_system.dir/worker_manager.cpp.obj.d"
+  "git_employee_management_system.exe"
+  "git_employee_management_system.exe.manifest"
+  "git_employee_management_system.pdb"
+  "libgit_employee_management_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_employee_management_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
